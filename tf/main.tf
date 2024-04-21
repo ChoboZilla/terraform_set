62fab601fcf07610ebc584e1681becea4a1089ac
ChoboZilla <100492375+ChoboZilla@users.noreply.github.com>
2024-04-21 18:45:00 +0300

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -19,7 +19,7 @@ provider "openstack" {
 }
 
 resource "openstack_networking_secgroup_v2" "sg" {
-  name        = "task_manager_sg"
+  name        = "task_managerN_sg"
 }
 
 resource "openstack_networking_secgroup_rule_v2" "sg_ssh_rule" {
